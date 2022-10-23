cd7863c0917a7dafca2730226be4d441d856d2c7
qfcoihnQfwasuhqnfqwas <inventiveworkmodel@gmail.com>
2022-10-23 02:57:17 -0700

    1

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function getInfo() {
 			window.location.href="user1.html"
 		}
 		if(username=="2" && password=="2"){
-			window.location.href="/user2/index.html"
+			window.location.href="user2.html"
 		}
 		if(username=="3" && password=="3"){
-			window.location.href="/user3/index.html"
+			window.location.href="user3.html"
 		}
 		if(username=="4" && password=="4"){
-			window.location.href="/user4/index.html"
+			window.location.href="user4.html"
 		}
 		
 	}
