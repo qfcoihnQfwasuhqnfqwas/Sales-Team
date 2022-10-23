c8dcd3fd630e11e0bf0cb0cc845ea8de1e8acfc2
qfcoihnQfwasuhqnfqwas <inventiveworkmodel@gmail.com>
2022-10-23 01:40:24 -0700

    1

---
+++ login/script.js
@@ -0,0 +1,19 @@
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username=="admin" && password=="admin"){
+			window.location.href="/user1/user.html"
+		}
+		if(username=="2" && password=="2"){
+			window.location.href="/user2/user.html"
+		}
+		if(username=="3" && password=="3"){
+			window.location.href="/user3/user.html"
+		}
+		if(username=="4" && password=="4"){
+			window.location.href="/user4/user.html"
+		}
+		
+	}
